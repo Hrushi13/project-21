62cff480d16ab35c460c4375b991e9ac370efce7
Hrushi13 <72509008+Hrushi13@users.noreply.github.com>
2021-04-14 17:09:17 +0530

    Add files via upload

---
+++ Project 20/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,37 @@
+var car,speed,wall,weight
+function setup() {
+  createCanvas(1600,400);
+  bullet = createSprite(50, 200, 50, 5);
+  thickness=random(22,83);
+  wall=createSprite(1200,200,thickness,height/2);
+  weight=random(30,52);
+  speed=random(223,321);
+  bullet.velocityX=speed;
+  bullet.shapeColor="white";
+wall.shapeColor="grey";
+}
+
+function draw() {
+  background(0);
+  if(hasCollided(bullet,wall)) {
+    bullet.velocityX=0
+    var damage=0.5*weight*speed*speed/thickness*thickness*thickness;
+    if (damage>10){
+      wall.shapeColor=color(255,0,0)
+    }
+    
+    if(damage<10){
+      wall.shapeColor=color(0,0,255);
+    }
+  }
+  drawSprites();
+}
+function hasCollided(bullet,wall){
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+
+if(bulletRightEdge>=wallLeftEdge){
+return true
+}
+return false
+}
